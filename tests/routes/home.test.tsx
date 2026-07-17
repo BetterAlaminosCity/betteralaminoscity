6030d47408cd7a395064789c68936c005de82af9
ljsalcedo-dev <ljsalcedo.dev@gmail.com>
2026-07-17 09:46:21 +0800

    test: move test files to top-level tests/ mirroring app/

--- tests/routes/home.test.tsx
+++ tests/routes/home.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import { describe, expect, it } from "vitest";
 
-import Home from "./home";
+import Home from "../../app/routes/home";
 
 describe("Home", () => {
   it("renders the Better Alaminos City heading", () => {
